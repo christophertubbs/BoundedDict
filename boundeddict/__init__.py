c3673ea75c1b85a2c236190e0c2cd7dc592b949a
Chris Tubbs <christopherotubbs@gmail.com>
2023-07-17 03:18:36 +0000

    Added the Entry class

--- boundeddict/__init__.py
+++ boundeddict/__init__.py
@@ -1,16 +1,29 @@
+"""
+A dictionary defined by ranges instead of discrete keys
+"""
 from __future__ import annotations
 import typing
+from typing import Any
 
 from .entry import Bound
 from .entry import Comparable
+from .entry import Entry
 
 _KT = typing.TypeVar("_KT", bound=Comparable)
 _T = typing.TypeVar("_T")
 
+_KEY = typing.Tuple[_KT, _KT]
 
-class BoundedDict(typing.Mapping[Bound[_KT], _T]):
+class BoundedDict(typing.Dict[Bound[_KT], _T]):
     """
     A dictionary that uses bounds for keys instead of discrete values
     """
     def __init__(self) -> None:
-        self.__entries: typing.List = list()
+        self.__entries: typing.List[Entry] = list()
+
+    def __getitem__(self, key: _KEY) -> _T:
+        pass
+    
+    def __setitem__(self, key: _KEY, value: _T):
+        pass
+
--- boundeddict/entry.py
+++ boundeddict/entry.py
@@ -1,11 +1,20 @@
+"""
+Defines the basic keys for the bounded dict
+"""
 from __future__ import annotations
 
 import typing
 
 T = typing.TypeVar("T")
+_KT = typing.TypeVar("_KT")
 
 
+@typing.runtime_checkable
 class Comparable(typing.Protocol, typing.Generic[T]):
+    """
+    Represents a generic type that implements comparison functions
+    """
+
     def __eq__(self, __value: object) -> bool:
         ...
 
@@ -21,12 +30,24 @@ class Comparable(typing.Protocol, typing.Generic[T]):
     def __le__(self, other: Comparable[T]) -> bool:
         ...
 
+    # TODO: Don't require +/-; Find another way to find the most accurate children
+    def __sub__(self, other: Comparable[T]) -> T:
+        ...
+
+    def __add__(self, other: Comparable[T]) -> T:
+        ...
+
 
 class Bound(typing.Generic[T]):
     """
     Defines the boundaries of the key
     """
-    def __init__(self, lower: T, upper: T):
+
+    def __init__(self, lower: Comparable[T], upper: Comparable[T]):
+        if not isinstance(lower, Comparable):
+            raise ValueError(
+                f"'{str(lower)}' may not be used as a lower bound - only comparable values are allowed"
+            )
         self.__lower: T = lower
         """The least acceptable value"""
 
@@ -39,17 +60,17 @@ class Bound(typing.Generic[T]):
         The least acceptable value
         """
         return self.__lower
-    
+
     @property
     def upper_bound(self) -> T:
         """
         The largest acceptable value
         """
         return self.__upper
-    
+
     def __contains__(self, value: T) -> bool:
         return self.lower_bound <= value <= self.upper_bound
-    
+
     def intersects_on_left(self, other: Bound[T]) -> bool:
         """
         Whether this bound intersects another bound on the left hand side
@@ -78,7 +99,7 @@ class Bound(typing.Generic[T]):
             Whether this bound intersects another bound on the left hand side
         """
         return other.lower_bound <= self.upper_bound <= other.upper_bound
-    
+
     def intersects_on_right(self, other: Bound[T]) -> bool:
         """
         Whether this bound intersects another bound on the right
@@ -130,12 +151,78 @@ class Bound(typing.Generic[T]):
 
         Returns:
             Whether this bound intersects with the other 
-        """ 
+        """
         return self.intersects_on_left(other) or self.intersects_on_right(other)
-    
+
+    def distance_from(self, lower_bound: _KT, upper_bound: _KT):
+        """
+        Determines the distance between one bound and another
+        """
+        return (lower_bound - self.lower_bound) + (self.upper_bound - upper_bound)
+
+
 
     def __str__(self):
         return f"[{self.lower_bound}, {self.upper_bound}]"
-    
+
     def __repr__(self) -> str:
         return str(self)
+
+
+class Entry(typing.Generic[T, _KT]):
+    """
+    Represents an individual value within a Bounded Dictionary
+    """
+    def __init__(self, lower_bound: _KT, upper_bound: _KT, value: T) -> None:
+        self.__bound: Bound[_KT] = Bound(lower=lower_bound, upper=upper_bound)
+        self.__value = value
+        self.__children: typing.List[Entry[T, _KT]] = list()
+
+    @property
+    def value(self) -> T:
+        """
+        The value that this entry contains
+        """
+        return self.__value
+
+    @property
+    def bounds(self) -> Bound[_KT]:
+        """
+        The bounds that dictates what may be found within this entry
+        """
+        return self.__bound
+
+    @property
+    def lower_bound(self) -> _KT:
+        """
+        The lower bound defining what may be in this entry
+        """
+        return self.__bound.lower_bound
+
+    @property
+    def upper_bound(self) -> _KT:
+        """
+        The lower bound defining what may be in this entry
+        """
+        return self.__bound.upper_bound
+    
+    @property
+    def search(self, lower_bound: _KT, upper_bound: _KT) -> typing.Optional[T]:
+        """
+        Look for a value nested within this entry
+        """
+        candidates: typing.Dict[Entry, int] = dict()
+
+        for child_entry in self.__children:
+            pass
+
+    def __contains__(self, bounds: typing.Tuple[_KT, _KT]) -> bool:
+        if not isinstance(bounds, typing.Sequence) or len(bounds) != 2:
+            raise ValueError(
+                f"'{str(bounds)}' cannot be used to check for containment - "
+                "value must be a sequence of two values marking a lower and upper bound"
+            )
+
+        lower_bound, upper_bound = bounds
+        return self.bounds.lower_bound <= lower_bound and upper_bound <= self.bounds.upper_bound
+    
